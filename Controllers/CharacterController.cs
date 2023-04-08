ab34fc22c48ea9e04dd5c630c82d776d1b5bb1b4
gajevski <mikolajxgajewski@gmail.com>
2023-04-08 20:01:06 +0200

    Add asynchrous calls

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -18,18 +18,18 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get() {
-            return Ok(_characterService.GetAllCharacters());
+        public async Task<ActionResult<List<Character>>> Get() {
+            return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id) {
-            return Ok(_characterService.GetCharacterById(id));
+        public async Task<ActionResult<Character>> GetSingle(int id) {
+            return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character NewCharacter) {
-            return Ok(_characterService.AddCharacter(NewCharacter));
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character NewCharacter) {
+            return Ok(await _characterService.AddCharacter(NewCharacter));
         }
     }
 }
