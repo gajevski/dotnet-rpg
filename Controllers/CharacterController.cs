1badcdd48575084352c2f1ae6ccd761b40a209da
gajevski <mikolajxgajewski@gmail.com>
2023-04-09 23:46:24 +0200

    Add generics to the service response

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -18,17 +18,17 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<List<Character>>> Get() {
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get() {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Character>> GetSingle(int id) {
+        public async Task<ActionResult<ServiceResponse<Character>>> GetSingle(int id) {
             return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<Character>>> AddCharacter(Character NewCharacter) {
+        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character NewCharacter) {
             return Ok(await _characterService.AddCharacter(NewCharacter));
         }
     }
