905de24b74bc443df868a69e0e379a0a1225b6cf
gajevski <mikolajxgajewski@gmail.com>
2023-04-11 13:37:19 +0200

    Implement update character methods

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -28,8 +28,17 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto NewCharacter) {
-            return Ok(await _characterService.AddCharacter(NewCharacter));
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter) {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> UpdateCharacter(UpdateCharacterDto updatedCharacter) {
+            var response = await _characterService.UpdateCharacter(updatedCharacter);
+            if (response.Data is null) {
+                return NotFound(response);
+            }
+            return Ok(response);
         }
     }
 }
