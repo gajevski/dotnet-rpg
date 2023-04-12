f72238ea42d44e1dcc7e7336880b22bc19f5057f
gajevski <mikolajxgajewski@gmail.com>
2023-04-12 10:48:28 +0200

    Add DELETE method

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -26,6 +26,28 @@ namespace dotnet_rpg.Services.CharacterService
             return serviceResponse;
         }
 
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+
+            try {
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            if (character is null) 
+                throw new Exception($"Character with Id '{id}' not found");
+
+                characters.Remove(character);
+
+                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            }
+
+            catch (Exception exception) {
+                serviceResponse.Success = false;
+                serviceResponse.Message = exception.Message;
+            }
+
+            return serviceResponse;
+        }
+
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
